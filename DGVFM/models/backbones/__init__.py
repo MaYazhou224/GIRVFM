1102c82b2343e2c244763c42fbad75bd58ed2264
MaYazhou <80452911+MaYazhou224@users.noreply.github.com>
2026-09-03 22:53:18 +0800

    Delete DGVFM directory

--- DGVFM/models/backbones/__init__.py
+++
@@ -1,5 +0,0 @@
-from .dino_v2 import DinoVisionTransformer
-from .dgvfm_dinov2 import DGVFMDinoVisionTransformer
-from .dgvfm_stem import DGVFM
-
-
--- DGVFM/models/heads/__init__.py
+++
@@ -1,3 +0,0 @@
-from .dgvfm_mask2former import DGVFMMask2FormerHead
-
-__all__ = ["DGVFMMask2FormerHead"]
